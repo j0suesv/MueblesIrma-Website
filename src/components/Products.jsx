42333776353d95dfbde3ab0f9cc61ba64f428f39
j0suesv <99438333+j0suesv@users.noreply.github.com>
2024-10-10 02:00:07 -0500

    products section was added

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import {products} from '../data';
+import ProductSlider from '../components/productSlider'
 
-const Products = () => {
-  return <div>Products</div>;
+function Products(){
+  const {title, subtitle} = products;
+
+  return <section className='section text-center'>
+    <div className="container mx-auto">
+      <div>
+        <h2 className='title'> {title} </h2>
+        <p className='max-w-[639px] mx-auto mb-[50px] lg:mb[70px]'> {subtitle} </p>
+      </div>
+
+      <div>
+        <ProductSlider />
+      </div>
+    </div>
+  </section>;
 };
 
 export default Products;
