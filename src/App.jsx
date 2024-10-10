3d2825e216ff0e71c4efdfbe19929fd8c094bb1b
j0suesv <99438333+j0suesv@users.noreply.github.com>
2024-10-10 01:02:28 -0500

    Hero, principal Features and new products sections  added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
-
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import NewItems from "./components/NewItems";
 
 function App() {
 
-  return (
-    <>
-    </>)
+  return  (
+  <div className='w-full max-w-[1440px] mx-auto bg-white '>
+    <Hero />
+    <Features />
+    <NewItems />
+
+  </div>
+)
 }
 
 export default App
---
+++ src/components/NewItemsSlider.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {newInStore} from '../data'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+function NewItemsSlider(){
+  return <Swiper grabCursor={true} breakpoints={{
+    320: {
+    slidesPerView: 2,
+    spaceBetween:18,
+  },
+  768: {
+    slidesPerView: 3,
+    spaceBetween: 20,
+  }
+  }}>
+    {newInStore.products.map((product, index) =>{
+      return <SwiperSlide className='max-w-[256px]' key={index}>
+        <div className='relative'>
+          <img src={product.image} alt="" />
+          <div className='absolute text-white bottom-[20px] w-full text-center text-[18px] lg:text-2xl font-medium capitalize'>{product.name}</div>
+        </div>
+
+      </SwiperSlide>
+
+    })}
+  </Swiper>;
+};
+
+export default NewItemsSlider;
